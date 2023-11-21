00234de280f66d9c41a2139c903ee72e17a0033f
Jithu-9847 <131839320+Jithu-9847@users.noreply.github.com>
2023-11-21 15:41:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,7 @@
 
 // ---------------------------------------------------animation-------------------------------------------------------------
 
-var t1=gsap.timeline();
-
-t1.from("#separate",
-  { 
-    y: -700,
-    opacity: 0,
-    duration:1
-  }
-   
-)
-t1.from("#bar img",
-  { 
-    x: -100,
-    opacity: 0,
-    duration:0.7,
-    stagger:0.7,
-    scale:0
-  }
-   
-)
-t1.from("#home_head",{
-    y:-100,
-    duration:0.7,
-    opacity:0,
-    
-})
-t1.from("#home_intro",{
-  x:-100,
-  duration:0.7,
-  opacity:0,
-  
-})
-t1.from("#web_logo_img",{
-  opacity:0,
-  scale:0
-})
-t1.from("#compose",{
-  opacity:0,
-  scale:0
-})
-
-t1.to("#plus",{
-  rotate:360,
-  delay:2,
-  duration:1,
-  repeat:4
-})
  
-gsap.from("#about1 h1,#about1 p",{
-  x:-100,
-  duration:1,
-  stagger:0.7,
-  opacity:0,
-})
 
 function  send() {
   if(document.getElementById('email').value!="" && document.getElementById('security').value!=""){
